1df3e9b1a42a5f21b6c016b0ed45e09738b3c7e3
pik <pik0329@naver.com>
2024-10-06 16:23:34 +0900

    asdasdasd

--- src/main/java/com/example/goat/controller/PlaceController.java
+++ src/main/java/com/example/goat/controller/PlaceController.java
@@ -1,17 +1,10 @@
 package com.example.goat.controller;
 
 import com.example.goat.dto.*;
-import com.example.goat.entity.Blog;
-import com.example.goat.entity.Place;
-import com.example.goat.entity.Vote;
-import com.example.goat.repository.BlogRepository;
-import com.example.goat.repository.PlaceRepository;
 import com.example.goat.repository.VoteRepository;
 import com.example.goat.service.BlogService;
-import com.example.goat.service.PlaceService;
 import com.example.goat.service.ReplyBlogService;
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -22,10 +15,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.security.Principal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Log4j2
@@ -36,48 +29,20 @@ public class PlaceController {
     private final BlogService blogService;
     private final ReplyBlogService replyBlogService;
     private final VoteRepository voteRepository;
-    private final BlogRepository blogRepository;
 
-    private final PlaceService placeService;
     @GetMapping("/register")
     public void register(BlogDTO blogDTO){
         log.info("등록 get 진입 완료");
     }
 
-
     @PostMapping("/register")
-    public String registerPost(@Valid BlogDTO blogDTO, BindingResult bindingResult, Model model, @AuthenticationPrincipal UserDetails user
-            , Long[] nnum
-    ){
-        log.info(blogDTO);
-        log.info(blogDTO);
-        log.info(blogDTO);
-        log.info(blogDTO);
+    public String registerPost(@Valid BlogDTO blogDTO, BindingResult bindingResult, Model model, @AuthenticationPrincipal UserDetails user){
+
 
         if(bindingResult.hasErrors()){
-            return "blog/register";
+            return "place/register";
         }
-        Long blognum= blogService.register(blogDTO, user);
-
-        if(nnum != null && nnum.length !=0){
-            for(Long num   : nnum){
-                log.info("저장된 장소값"+ num);
-                //기존 저장된 장소의 값을 가져오는 service(장소num, 본문부모번호blognum)
-
-                try {
-                    Place place = placeService.readOne(num, blognum);
-
-                    log.info("수정된 장소정보 : " + place);
-                } catch (EntityNotFoundException e) {
-                    model.addAttribute("msg", "장소정보를 찾을수 없습니다.");
-                    return "place/register";
-                }
-
-                //sㅓ비스에서는 장소넘으로 저장된 장소엔티티를 찾고
-                // 저장된 부모엔티티를 찾아서 이결과를
-                //장소넘에 있는 set부모(이결과에)   하면 @tran젝션이 있으면 자동 업데이트
-            }
-        }
+        blogService.register(blogDTO, user);
         return "redirect:/place/list";
     }
 
@@ -103,45 +68,37 @@ public class PlaceController {
 
         model.addAttribute("blogDTO", blogService.detale(num));
         RPageResponseDTO<ReplyBlogDTO> rPageResponseDTO =  replyBlogService.list(rPageRequestDTO, num);
-//        log.info("값이 있는지"+rPageResponseDTO);
+        log.info("값이 있는지"+rPageResponseDTO);
         model.addAttribute("replyBlogDTO",new ReplyBlogDTO());
         model.addAttribute("rPageResponseDTO",rPageResponseDTO);
-//        log.info("get 디테일 진입 완료");
-
-        if(user != null){
-            if(voteRepository.voteCheck(user.getUsername(),num)==null){
-                log.info("1번으로 체크됨");
-                model.addAttribute("voteCheck", "1");
-            }else {
-                model.addAttribute("voteCheck","2");
-                log.info("2번으로 체크됨");
-            }
-
-
-
+        log.info("get 디테일 진입 완료");
+        if(voteRepository.voteCheck(user.getUsername(),num)==null){
+            log.info("1번으로 체크됨");
+            model.addAttribute("voteCheck", "1");
         }
 
-
+        else model.addAttribute("voteCheck","2");
+        log.info("2번으로 체크됨");
 
 
     }
 
     @GetMapping("/delete")
     public String delete(Model model, Long num){
-        blogService.delete(num);
-        return "redirect:/place/list";
+       blogService.delete(num);
+       return "redirect:/place/list";
     }
     @GetMapping("/modify")
     public String modify(Model model, Long num){
         model.addAttribute("blogDTO", blogService.detale(num));
-        return "blog/modify";
+        return "place/modify";
     }
     @PostMapping("/modify")
     public String modify(@Valid BlogDTO blogDTO, BindingResult bindingResult){
 
 
         if(bindingResult.hasErrors()){
-            return "blog/modify";
+            return "place/modify";
         }
         blogService.modify(blogDTO);
         return "redirect:/place/list";
@@ -154,7 +111,7 @@ public class PlaceController {
         log.info("유저"+user);
         blogService.vote(num, user, voteDTO);
 
-        return "redirect:/blog/detale?num="+num;
+        return "redirect:/place/detale?num="+num;
     }
 
     @ResponseBody
@@ -174,25 +131,4 @@ public class PlaceController {
         return new ResponseEntity<String>("success", HttpStatus.OK);
 
     }
-
-    @ResponseBody
-    @PostMapping("/savePlaces")
-    public   ResponseEntity savePlaces(@RequestBody PlaceDTO dto ){
-
-        log.info(dto);
-
-        Long result =  placeService.saveplace(dto);
-
-        if(result == null){
-            return new ResponseEntity<String>("야 틀렸어", HttpStatus.BAD_REQUEST);
-
-        }
-
-
-        return new ResponseEntity<Long>(result, HttpStatus.OK);
-
-    }
-
-
-
 }
